ea7195dde2d323106e0cb4c641dcfd1a1925a03c
Ela <micaelafrancisco13@outlook.com>
2025-05-10 19:25:37 +0800

    08 - Running Spring Boot apps

--- store/src/main/java/com/codewithmosh/store/StoreApplication.java
+++ store/src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -6,29 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StoreApplication {
 
-    /*
-     * In this lesson, we'll create our first controller to handle requests to the home page
-     * and serve a simple HTML file.
-     * But before we start coding, let's take a moment to understand
-     * the basics of Spring MVC, which is the foundation of how we handle web requests in Spring Boot.
-     *
-     * Spring MVC stands for Model-View-Controller.
-     * It's part of the Spring Framework that helps us build web applications.
-     * It provides a clean way to separate different parts of our application,
-     * making it easier to manage and scale.
-     *
-     * The Model is where our application's data lives.
-     * It represents the business logic and is usually connected to a database
-     * or other data sources.
-     * In Spring Boot, the model can be a simple Java class.
-     *
-     * The View is what the user sees.
-     * It's the HTML, CSS, or JavaScript rendered in the browser.
-     * In Spring MVC, views can be static files or dynamically generated using tools like Thymeleaf.
-     *
-     * The Controller is like a traffic controller.
-     * It handles incoming requests from the user, interacts with the model to get data,
-     * and then tells the view what to display.
+    /**
+     * If you're not using IntelliJ, you can run this application using Maven.
+     * First, we have to open a terminal window and go to our project folder.
+     * If you don't have Maven globally installed on your machine, you can use the Maven wrapper.
+     * With this Maven wrapper, we can run our Spring Boot application using `./mvnw spring-boot:run`.
      */
 
     public static void main(String[] args) {
--- store/src/test/java/com/codewithmosh/store/HomeController.java
+++ store/src/test/java/com/codewithmosh/store/HomeController.java
@@ -4,42 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*
-To make this a controller, we have to decorate it with the @Controller annotation.
-An annotation is like a label or a tag that we add in the code to give instructions or additional context
-to the compiler. We can apply them to classes, methods, fields, parameters, and so on.
-
-So in this example, by adding this annotation to this class, we are telling Spring that this class should
-be used as a web controller
-for receiving web traffic.
-*/
 @Controller
-/*
-Now when we send a request to the root of our website, we want this method to be called. To do that, we have
-to apply another special annotation here that is @RequestMapping.
-
-This annotation is defined in the package org.springframework.web. This is part of the web starter dependency
-that we added in the previous lesson.
-
-So we add this annotation and then give it an argument. That is a URL pattern. Here, we can type a forward
-slash that represents the root of our website. So when a request goes to the root of our website, this
-method gets called. If you want to receive traffic at a different endpoint like "about" or "contact," we can
-change the argument here.
-*/
 @RequestMapping("/")
 public class HomeController {
     @GetMapping()
     public String index() {
-        /*
-        In this method, we return the name of the view that should be returned to the browser. So we return
-        "index.html."
-
-        Now we need to create this view. Back to our project. We go to the resources' folder. This is where we
-        add non-Java files.
-
-        Now here we add a new folder called "static." And inside the static folder, we add a new file. Well, a
-        new HTML file called "index.html."
-        */
         return "index.html";
     }
 }
