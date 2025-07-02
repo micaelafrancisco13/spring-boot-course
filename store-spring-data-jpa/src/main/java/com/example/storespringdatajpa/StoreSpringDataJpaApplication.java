5c9328babf2eaf66368c30f0530c7107c9b0b985
Ela <micaelafrancisco13@outlook.com>
2025-07-02 19:07:58 +0800

    add store-spring-data-jpa module

---
+++ store-spring-data-jpa/src/main/java/com/example/storespringdatajpa/StoreSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.storespringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ store-spring-data-jpa/src/test/java/com/example/storespringdatajpa/StoreSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storespringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
