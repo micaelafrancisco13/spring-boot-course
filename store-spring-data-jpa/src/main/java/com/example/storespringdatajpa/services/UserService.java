1c390ba920344f0f0d4b6d444eb329db86262d1c
Ela <micaelafrancisco13@outlook.com>
2025-07-05 14:44:00 +0800

    managing transactions
    commit

---
+++ store-spring-data-jpa/src/main/java/com/example/storespringdatajpa/services/UserService.java
@@ -0,0 +1,45 @@
+package com.example.storespringdatajpa.services;
+
+import com.example.storespringdatajpa.entities.User;
+import com.example.storespringdatajpa.repositories.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    // this EntityManager is responsible for managing entities using a persistence context
+    private final EntityManager em;
+
+    @Transactional
+    public void showEntityStates() {
+        // these 2 transactions from userRepository: findById() and delete() have their
+        // own persistence context. while the transaction is in progress, we have a
+        // persistence context.
+
+        // what if we want to tie a transaction boundary not only in the repository methods
+        // (e.g., save()) but inside a n entire method for multiple transactions?
+        // use the method-level @Transactional annotation
+
+        var user = new User();
+        user.setName("Ela");
+        user.setEmail("ela13@gmail.com");
+        user.setPassword("Password13!");
+
+        // check if the object is already on the persistence context
+        if (em.contains(user)) System.out.println("Persistent");
+        else System.out.println("Transient or Detached");
+
+        // all the methods of a repository are transactional
+        userRepository.save(user);
+        userRepository.findAll().forEach(u -> System.out.println(u.getEmail()));
+
+        if (em.contains(user)) System.out.println("Persistent");
+        else System.out.println("Transient or Detached");
+
+    }
+}
