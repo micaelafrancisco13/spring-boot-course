a51ac47c23b9c893128aa93fd604769a98e4de1a
Ela <micaelafrancisco13@outlook.com>
2025-07-07 14:30:06 +0800

    reformat code

--- store-spring-data-jpa/src/main/java/com/example/storespringdatajpa/StoreSpringDataJpaApplication.java
+++ store-spring-data-jpa/src/main/java/com/example/storespringdatajpa/StoreSpringDataJpaApplication.java
@@ -4,8 +4,6 @@ import com.example.storespringdatajpa.services.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-
 @SpringBootApplication
 public class StoreSpringDataJpaApplication {
 
